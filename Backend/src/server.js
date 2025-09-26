68c9827a396b2500727abac1a94f1745a09f27af
Vansh Soni <vanshsoni13062005@gmail.com>
2025-09-26 11:03:24 +0530

    fixed issues

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -5,32 +5,10 @@ const handleAi = require("./handleAi");
 const app = express();
 app.use(express.json());
 
-// app.use(cors({
-//   origin: ['',''],
-//   credentials: true
-// }));
-
-const allowedOrigins = [
-  'http://localhost:1234',           // local dev
-  'https://krishisakhi1.netlify.app' // production frontend
-];
-
-// CORS options
-const corsOptions = {
-  origin: function (origin, callback) {
-    // Allow requests with no origin (like mobile apps, Postman)
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
-};
-
-app.use(cors(corsOptions));
+app.use(cors({
+  origin: ['http://localhost:1234','https://krishisakhi1.netlify.app'],
+  credentials: true
+}));
 
 app.post("/handleAi", async (req, res) => {
   try {
